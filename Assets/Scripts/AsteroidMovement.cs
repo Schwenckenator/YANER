2e9e9070cda904c50b930b3c6a1551d8f1d4f659
Matthew Schwencke <matt.schwencke@gmail.com>
2014-04-28 23:50:31 +1000

    - Added Game end. This occurs after 4 asteroid hits - Fixed Asteroids hitting the walls to the side
    Signed-off-by: Matthew Schwencke <matt.schwencke@gmail.com>

--- Assets/Scripts/AsteroidMovement.cs
+++ Assets/Scripts/AsteroidMovement.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class AsteroidMovement : MonoBehaviour {
 	GameManager manager;
+	ShipCondition playerShip;
 	// Use this for initialization
 	void Start () {
 		manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+		playerShip = GameObject.FindGameObjectWithTag("PlayerShip").GetComponent<ShipCondition>();
 
 		Vector3 newForce = new Vector3(0, 0, -1 * manager.GetCurrentAstSpeed());
 		rigidbody.AddForce(newForce);
@@ -14,14 +16,20 @@ public class AsteroidMovement : MonoBehaviour {
 
 	IEnumerator CheckForDeath(){
 		while(true){
-			if(transform.position.z < -1.0f){
+			if(transform.position.z < -5.0f){
 				Destroy (gameObject);
 			}
 			yield return new WaitForSeconds(1.0f);
 		}
 	}
 	void OnTriggerEnter(Collider col){
-		manager.ReduceCurrentSpeed(25); // Percentage reduction here
-		Destroy (gameObject);
+		if(col.CompareTag("PlayerShip")){
+			manager.ReduceCurrentSpeed(25); // Percentage reduction here
+			playerShip.TakeDamage(1); // For now, each asteroid deals 1 damage
+		}
+		if(!col.CompareTag("Box")){
+			Debug.Log (col.tag);
+			Destroy (gameObject);
+		}
 	}
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,20 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 	public GameObject Asteroid;
 	public int StartingAsteroidSpeed;
-	private int currentAstSpeed;
-	private int highestAstSpeed;
+	public float AsteroidSpawnPosition; // How far back asteroids spawn
+	public float AsteroidHorizontalSpawnPosition; // How far either side the asteroids can spawn
+
 	public float minTime;
 	public float maxTime;
+
+	public float minScale;
+	public float maxScale;
+
+	private int currentAstSpeed;
+	private int highestAstSpeed;
+
+	public bool gameFinished = false;
+
 	// Use this for initialization
 	void Start () {
 		currentAstSpeed = StartingAsteroidSpeed;
@@ -16,15 +26,23 @@ public class GameManager : MonoBehaviour {
 	}
 
 	void FixedUpdate(){
-		currentAstSpeed++;
-		if(currentAstSpeed > highestAstSpeed){
-			highestAstSpeed = currentAstSpeed;
+		if(!gameFinished){
+			currentAstSpeed++;
+			if(currentAstSpeed > highestAstSpeed){
+				highestAstSpeed = currentAstSpeed;
+			}
 		}
 	}
 
 	IEnumerator SpawnAsteroid(){
-		while(true){
-			Instantiate(Asteroid, new Vector3(Random.Range (-5,5), 0, 50), new Quaternion());
+		while(!gameFinished){
+			GameObject newAsteroid = Instantiate(Asteroid, 
+			                                     new Vector3(Random.Range (-AsteroidHorizontalSpawnPosition,AsteroidHorizontalSpawnPosition), 0, AsteroidSpawnPosition), 
+			                                     new Quaternion()) as GameObject;
+			float newScaleF = Random.Range(minScale, maxScale);
+			Vector3 newScaleV3 = new Vector3(newScaleF, newScaleF, newScaleF);
+			newAsteroid.transform.localScale = newScaleV3;
+
 			yield return new WaitForSeconds(Random.Range(minTime/(float)currentAstSpeed, maxTime/(float)currentAstSpeed));
 		}
 	}
@@ -38,7 +56,6 @@ public class GameManager : MonoBehaviour {
 		// Reduces as a percentage, minus 1000 base speed
 		int extraSpeed = currentAstSpeed - 1000;
 		int speedReduction = Mathf.RoundToInt(extraSpeed * (amount)*0.01f);
-		Debug.Log (speedReduction.ToString());
 		currentAstSpeed -= speedReduction;
 	}
 }
