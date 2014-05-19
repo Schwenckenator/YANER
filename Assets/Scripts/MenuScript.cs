153fc9057010272772c360b30ad15471f1165c8a
Nathan <nathanjbrown@live.com>
2014-05-19 21:12:46 +1000

    Update MenuScript.cs

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,8 +1,8 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class MenuScript : MonoBehaviour {
-
+	
 	Rect menuRect = new Rect(Screen.width/4, Screen.height/4, Screen.width/2,Screen.height/2);
 
 	void OnGUI(){
@@ -10,7 +10,29 @@ public class MenuScript : MonoBehaviour {
 
 	}
 	void BasicMenu(int windowID){
-		if(GUI.Button(new Rect(20, 20, 100, 20), "Play Game")){
+		//buttons for multiple levels, should be unlocked by playing, each level can use same scene
+		//GameManager can use arrays for things like level complete distances and level achievements
+		if(GUI.Button(new Rect(20, 20, 100, 20), "Continue Game")){
+			Application.LoadLevel("Test Scene");
+		}
+		if(GUI.Button(new Rect(140, 20, 100, 20), "Play Level 1")){
+			PlayerPrefs.SetInt("Level",1);
+			Application.LoadLevel("Test Scene");
+		}
+		if(GUI.Button(new Rect(260, 20, 100, 20), "Play Level 2")){
+			PlayerPrefs.SetInt("Level",2);
+			Application.LoadLevel("Test Scene");
+		}
+		if(GUI.Button(new Rect(380, 20, 100, 20), "Play Level 3")){
+			PlayerPrefs.SetInt("Level",3);
+			Application.LoadLevel("Test Scene");
+		}
+		if(GUI.Button(new Rect(500, 20, 100, 20), "Play Level 4")){
+			PlayerPrefs.SetInt("Level",4);
+			Application.LoadLevel("Test Scene");
+		}
+		if(GUI.Button(new Rect(620, 20, 100, 20), "Play Level 5")){
+			PlayerPrefs.SetInt("Level",5);
 			Application.LoadLevel("Test Scene");
 		}
 	}
